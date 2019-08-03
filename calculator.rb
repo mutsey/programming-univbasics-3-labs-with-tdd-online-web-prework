f2d2fb1e4378ccf22efbfe7885a21457f67ffe79
Meagan Utsey <meagan.utsey@gmail.com>
2019-08-03 20:47:29 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,14 @@
-# Add your variables here
+./calculator.rb
+  contains a local variable called first_number that is assigned to a number (FAILED - 1)
+ 
+Failures:
+ 
+  1) ./calculator.rb contains a local variable called first_number that is assigned to a number
+     Failure/Error: expect(first_number).to be_an(Integer).or be_a(Float)
+ 
+          expected "Hello world!" to be a kind of Integer
+ 
+       ...or:
+ 
+          expected "Hello world!" to be a kind of Float
+     # ./spec/calculator_spec.rb:8:in `block (2 levels) in <top (required)>'
